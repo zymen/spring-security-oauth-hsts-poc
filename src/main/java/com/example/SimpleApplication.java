46da056e369fe4beb2ed4d982e5e4c3922f2ed80
Rob Winch <rwinch@users.noreply.github.com>
2018-02-09 10:14:55 -0600

    Disable Strict-Transport-Security for OAuth endpoints

--- src/main/java/com/example/SimpleApplication.java
+++ src/main/java/com/example/SimpleApplication.java
@@ -2,12 +2,9 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 @EnableWebSecurity
-@EnableAuthorizationServer
 @SpringBootApplication
 public class SimpleApplication {
 
--- src/test/java/com/example/SimpleApplicationTests.java
+++ src/test/java/com/example/SimpleApplicationTests.java
@@ -2,15 +2,27 @@ package com.example;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class SimpleApplicationTests {
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
-	public void contextLoads() {
+	public void oauth() throws Exception {
+		this.mockMvc.perform(get("/oauth/token").secure(true))
+				.andExpect(header().doesNotExist("Strict-Transport-Security"));
 	}
 
 }
